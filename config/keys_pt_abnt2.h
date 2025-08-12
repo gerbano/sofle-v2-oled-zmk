34bc6ff90ad7deb31c0d821aa667296def17be08
Gabriel Erbano <gerbano@gmail.com>
2025-08-12 16:05:54 -0300

    Update keys_pt_abnt2.h

--- config/keys_pt_abnt2.h
+++ config/keys_pt_abnt2.h
@@ -56,6 +56,7 @@
 #include <dt-bindings/zmk/hid_usage.h>
 #include <dt-bindings/zmk/hid_usage_pages.h>
 #include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/pointing.h>
 
 #define PT_ESCAPE_CHARACTER (LC(ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_RIGHT_BRACKET_AND_RIGHT_BRACE)))
 
